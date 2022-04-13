239775ac87e9d0dad93055289b39603142842d9b
Safa <safa.howaid@gmail.com>
2022-04-13 15:18:55 -0400

    Add notch phone support

--- components/HandPumpType.tsx
+++ components/HandPumpType.tsx
@@ -4,20 +4,20 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 export default function HandPumpType() {
   return (
     <TouchableOpacity style={styles.button}>
-        <Image source={require('../assets/imk2.png')}/>
-        <Text>India Mark II</Text>
+      <Image source={require("../assets/imk2.png")} />
+      <Text>India Mark II</Text>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        width: 90,
-        height: 105, 
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "white",
-        borderRadius: 10,
-      },
-  });
-  
+  button: {
+    width: 90,
+    height: 105,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: 10,
+    marginRight: 10,
+  },
+});
--- components/topbar.tsx
+++ components/topbar.tsx
@@ -1,39 +1,33 @@
-import { Text,StatusBar, View, StyleSheet } from "react-native";
+import { Text, StatusBar, View, StyleSheet } from "react-native";
 import Colours from "../constants/Colours";
 import Icon from "../components/icon";
 // add it so that instead it takes a function for the name
 interface Props {
-    title: string;
+  title: string;
 }
 export default function TopBar(props: Props) {
   return (
-      
     <View style={styles.topbar}>
-       
-            <StatusBar barStyle="dark-content"/>
-            <Text style={styles.text}>{props.title}</Text>
-            <Icon name="JD"/>
-       
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.text}>{props.title}</Text>
+      <Icon name="JD" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    text : {
-        fontFamily: 'SFProText-Bold',
-        fontSize: 26,
-    },
-    topbar: {
-        width: '100%',
-        height: '15%',
-        paddingLeft:'5%',
-        paddingRight: '5%',
-        paddingTop: '15%',
-        backgroundColor: Colours.white, 
-        flexDirection:'row',
-        justifyContent: "space-between",
-     
-    },
- 
-  });
-  
+  text: {
+    fontFamily: "SFProText-Bold",
+    fontSize: 26,
+  },
+  topbar: {
+    width: "100%",
+    height: "10%",
+    paddingLeft: "5%",
+    paddingRight: "5%",
+    backgroundColor: Colours.white,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
