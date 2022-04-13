d99f4a778baa774e6134758afb992a3e431d576b
Safa <safa.howaid@gmail.com>
2022-04-13 17:09:30 -0400

    Add database object data type

--- api/schemas.tsx
+++ api/schemas.tsx
@@ -1,4 +1,4 @@
-import { object, string, number, InferType } from "yup";
+import { object, string, number, InferType, array } from "yup";
 
 export let watersourceSchema = object({
   id: string(),
@@ -19,4 +19,37 @@ export let watersourceSchema = object({
   geohash: string(),
 }).required();
 
+export let userSchema = object({
+  id: string(),
+  name: string(),
+}).required();
+
+export let commentSchema = object({
+  id: string(),
+  text: string(),
+  likes: number(),
+  author: object({ userSchema }),
+}).required();
+
+export let postSchema = object({
+  id: string(),
+  question: string(),
+  answer: string(),
+  comments: array().of(commentSchema),
+}).required();
+
+export let handPumpGuideSchema = object({
+  id: string(),
+  name: string(),
+  quick_fixes: array().of(postSchema),
+  pump_head: array().of(postSchema),
+  handle: array().of(postSchema),
+  pump_stand: array().of(postSchema),
+  pump_rods: array().of(postSchema),
+}).required();
+
 export type WaterSource = InferType<typeof watersourceSchema>;
+export type Post = InferType<typeof postSchema>;
+export type HandPumpGuide = InferType<typeof handPumpGuideSchema>;
+export type Comment = InferType<typeof commentSchema>;
+export type User = InferType<typeof userSchema>;
