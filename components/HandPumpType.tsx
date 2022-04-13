4999129e5f35d20873a33c0de26caf01469e08d8
dokzai <38355190+dokzai@users.noreply.github.com>
2022-04-13 16:44:48 -0400

    added dynamic pictures

--- components/HandPumpType.tsx
+++ components/HandPumpType.tsx
@@ -1,11 +1,67 @@
 import { Text, Image, View, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-export default function HandPumpType() {
+interface Props {
+  handpump_type: string;
+}
+const handpumps = {
+  india_mark_ii: {
+    handpump_name: 'India Mark II', 
+    uri: require('../assets/imk2.png')
+  },
+  afridev: {
+    handpump_name: 'Afridev', 
+    uri: require('../assets/afridev.png')
+  },
+  kardia: {
+    handpump_name: 'Kardia', 
+    uri: require('../assets/Kardia.png')
+  },
+  vergnet: {
+    handpump_name: 'Vergnet', 
+    uri: require('../assets/Vergnet.png')
+  },
+  volanta: {
+    handpump_name: 'Volanta', 
+    uri: require('../assets/Volanta.png')
+  },
+  nira: {
+    handpump_name: 'Nira', 
+    uri: require('../assets/Nira.png')
+  }
+}
+
+export default function HandPumpType(props : Props) {
+  let handpump = null;
+
+  switch(props.handpump_type) {
+    case "imk2":
+      handpump = handpumps.india_mark_ii;
+      break;
+    case "afridev":
+      handpump = handpumps.afridev;
+      break;
+    case "nira":
+      handpump = handpumps.nira;
+      break;
+    case "volanta":
+      handpump = handpumps.volanta;
+      break;
+    case "vergnet":
+      handpump = handpumps.vergnet;
+      break;
+    case "kardia":
+      handpump = handpumps.kardia;
+      break;
+    default:
+      handpump = handpumps.india_mark_ii;
+      break;
+  }
+
   return (
     <TouchableOpacity style={styles.button}>
-      <Image source={require("../assets/imk2.png")} />
-      <Text>India Mark II</Text>
+      <Image style={styles.scale_image} source={handpump.uri} />
+      <Text style={styles.pump_name}>{handpump.handpump_name}</Text>
     </TouchableOpacity>
   );
 }
@@ -20,4 +76,13 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginRight: 10,
   },
+  scale_image: {
+    maxWidth:'50%',
+    maxHeight:'50%',
+  },
+  pump_name: {
+    marginRight: 15,
+    marginLeft: 15,
+    textAlign: 'center'
+  }
 });
